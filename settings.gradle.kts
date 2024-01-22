b0882549f70cd0e69134aa918db95a36388fb3ef
Asst2Mangr <124299473+Asst2Mangr@users.noreply.github.com>
2024-01-22 10:26:11 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TeacherTalk"
+include(":app")
