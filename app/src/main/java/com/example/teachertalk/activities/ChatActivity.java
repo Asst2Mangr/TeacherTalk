dcff67302b869cdb90d64d9f3c0d5948aae6fb69
Levi-Schoonhoven <levi.h.schoonhoven@gmail.com>
2024-02-24 18:53:37 -0600

    video 7

---
+++ app/src/main/java/com/example/teachertalk/activities/ChatActivity.java
@@ -0,0 +1,18 @@
+package com.example.teachertalk.activities;
+
+import static com.example.teachertalk.R.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.teachertalk.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_chat);
+    }
+}
